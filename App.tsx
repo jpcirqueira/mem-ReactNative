bc18369599a64b87a13613d31388a4e8a97fb2d4
jpcirqueira <jpcirqueira81@gmail.com>
2021-07-05 14:26:01 -0300

    create global styles.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import theme from './src/global/theme';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  })
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
   return (
-    <View>
-      <Text>teste!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>teste!</Text>
+      </View>
+    </ThemeProvider>
+
   );
 }
---
+++ src/global/theme.ts
@@ -0,0 +1,18 @@
+export default {
+  colors: {
+    primary: '#312E38',
+    primary_dark: '#28262E',
+    secondary: '#FF9000',
+    sucess: '#12A454',
+    falied: '#DF2800',
+    shape: '#FFFFFF',
+    text: '#969CB2',
+    background: '#3E3B47',
+  },
+
+  fonts: {
+    regular: 'Roboto_400Regular',
+    medium: 'Roboto_500Medium',
+    bold: 'Roboto_700Bold',
+  }
+}
