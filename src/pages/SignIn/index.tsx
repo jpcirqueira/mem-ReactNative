a395ff35d6c356f029d1c7c9057d0ce49b27bc57
jpcirqueira <jpcirqueira81@gmail.com>
2021-07-05 16:20:46 -0300

    create page SignIn.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { Container } from './styles';
+import { 
+  Container,
+  Form,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  NewAccount,
+  Icon,
+  NewAccountText,
+} from './styles';
 
 function SignIn() {
 
@@ -10,7 +20,19 @@ function SignIn() {
   }
   return (
     <Container>
-      <Button text='Entrar' onPress={teste}/>
+      <Form>
+        <Title>Faça seu Login</Title>
+        <Input placeholder='email' icon='teste'/>
+        <Input placeholder='senha' icon='teste'/>
+        <Button text='entrar'onPress={teste}/>
+        <ForgotPassword>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Form>
+      <NewAccount>
+        <Icon name='log-in'/>
+        <NewAccountText>Criar uma conta</NewAccountText>
+      </NewAccount>
     </Container>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,53 @@
 import styled from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const Form = styled.View`
+  width: 100%;
   justify-content: center;
   align-items: center;
+  margin-top: ${RFPercentage(30)}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(25)}px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  margin-bottom: 10px;
+
+`;
+
+export const ForgotPassword = styled(RectButton)`
+
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  margin-top: 8px;
+`;
+
+export const NewAccount = styled(RectButton)`
+  flex-direction: row;
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(25)}px;
+  margin-right: 5px;
+`;
+
+export const NewAccountText = styled.Text`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(17)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
 `;
